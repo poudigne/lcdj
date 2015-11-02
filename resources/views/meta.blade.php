5cf14b35c0e2d5d507fd260aae3d2cc20baf44d6
poudigne <pierluc0414@live.ca>
2015-11-01 21:57:02 -0500

    Added noUiSlider libs to create slider to choose the age rang and player min-max numbers

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -1,16 +1,15 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<!-- Latest compiled and minified CSS -->
-<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">-->
 <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
-
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
-          
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--  noUiSlider -->
+<link href="noUiSlider/nouislider.min.css" rel="stylesheet">
+<script src="assets/noUiSlider/nouislider.min.js"></script>
+
 
 <style>
   
