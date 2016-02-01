789fd888a0bbcfca74dca833f9866238ba134761
poudigne <pierluc0414@live.ca>
2016-02-01 18:41:20 -0500

    Added migration file for Sale database and link between sale and product

---
+++ database/migrations/2016_02_01_233102_create_sale_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('product_sale', function (Blueprint $table) {
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->integer('sale_id')->unsigned()->index();
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_sale');
+        Schema::drop('sales');
+    }
+}
